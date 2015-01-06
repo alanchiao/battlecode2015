ebb9931e45a67ef0a3784a949cab5226f6e3979c
George Du <gdu@mit.edu>
2015-01-06 14:58:15 -0500

    fixed beaver

--- units/Beaver.java
+++ units/Beaver.java
@@ -7,6 +7,7 @@ import battlecode2015.utils.DirectionHelper;
 public class Beaver extends Robot {
 	public static void move() {
 		try {
+			MapLocation myLocation = rc.getLocation();
 			RobotInfo[] enemies = rc.senseNearbyRobots(rc.getType().attackRadiusSquared, rc.getTeam().opponent());
 			if (enemies.length >0 && rc.isWeaponReady()) { 
 				if (rc.isWeaponReady()) {
@@ -15,17 +16,16 @@ public class Beaver extends Robot {
 					}
 				}
 				else {
-					rc.move(rc.getLocation().directionTo(enemies[0].location).opposite());	
+					rc.move(myLocation.directionTo(enemies[0].location).opposite());	
 				}
 			}
 			if (rc.isCoreReady()) {
-				// HQ has given command for this particular
-				// beaver to build a barracks
+				// HQ has given command for this particular beaver to build a barracks
 				if (rc.readBroadcast(200) == rc.getID()) {
 					rc.broadcast(200, 0);
 					int offsetIndex = 0;
 					int[] offsets = {0,1,-1,2,-2,3,-3,4};
-					int dirint = DirectionHelper.directionToInt(rc.getLocation().directionTo(rc.senseHQLocation()));
+					int dirint = DirectionHelper.directionToInt(myLocation.directionTo(rc.senseHQLocation()));
 					while (offsetIndex < 8 && !rc.canMove(DirectionHelper.directions[(dirint+offsets[offsetIndex]+8)%8])) {
 						offsetIndex++;
 					}
@@ -37,17 +37,25 @@ public class Beaver extends Robot {
 						rc.build(buildDirection, RobotType.BARRACKS);
 					}
 				} 
-				
-				// looks around for an ore concentration that is bigger than it's current location by a certain fraction
-				for (Direction dir: DirectionHelper.directions) {
-					if(rc.senseOre(rc.getLocation().add(dir)) > (double)3/2*rc.senseOre(rc.getLocation())  && rc.canMove(dir)) {
-						rc.move(dir);
+				else {
+					double currentOre = rc.senseOre(myLocation);
+					double maxOre = -2;
+					Direction bestDirection = null;
+					// looks around for an ore concentration that is bigger than its current location by a certain fraction
+					for (Direction dir: DirectionHelper.directions) {
+						double possibleOre = rc.senseOre(myLocation.add(dir));
+						if (possibleOre > maxOre && rc.canMove(dir)) {
+							maxOre = possibleOre;
+							bestDirection = dir;
+						}
+					}
+					if (maxOre > 1.5 * currentOre) {
+						rc.move(bestDirection);
+					}
+					else {
+						rc.mine();
 					}
 				}
-				if (rc.senseOre(rc.getLocation()) == 0) {
-					rc.move(Direction.EAST);
-				}
-				rc.mine();
 			}
 		} catch (Exception e) {
 			System.out.println("Beaver Exception");
