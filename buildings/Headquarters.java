1550074dc8453176e6b127efc6e3ede6e79ef774
George Du <gdu@mit.edu>
2015-01-05 21:20:26 -0500

    modularity shell

---
+++ buildings/Headquarters.java
@@ -0,0 +1,9 @@
+package battlecode2015.buildings;
+
+import battlecode.common.*;
+
+public class Headquarters {
+	public static void move(RobotController rc) {
+		
+	}
+}
---
+++ units/Beaver.java
@@ -0,0 +1,9 @@
+package battlecode2015.units;
+
+import battlecode.common.*;
+
+public class Beaver {
+	public static void move(RobotController rc) {
+		
+	}
+}
