d92d9a2da0e59883c7f1fc121a52b7c90ac663c4
dchoi2 <dchoi2@mit.edu>
2015-01-06 11:45:21 -0500

    beavercode

--- units/Beaver.java
+++ units/Beaver.java
@@ -7,20 +7,18 @@ import battlecode2015.utils.DirectionHelper;
 public class Beaver extends Robot {
 	public static void move() {
 		try {
-			if (rc.isWeaponReady()) {
-				RobotInfo[] enemies = rc.senseNearbyRobots(rc.getType().attackRadiusSquared, rc.getTeam().opponent());
-				if (enemies.length > 0) {
-					rc.attackLocation(enemies[0].location);
+			RobotInfo[] enemies = rc.senseNearbyRobots(rc.getType().attackRadiusSquared, rc.getTeam().opponent());
+			if (enemies.length >0 && rc.isWeaponReady()) { 
+				if (rc.isWeaponReady()) {
+					if (enemies.length == 1) {
+						rc.attackLocation(enemies[0].location);	
+					}
+				}
+				else {
+					rc.move(rc.getLocation().directionTo(enemies[0].location).opposite());	
 				}
 			}
 			if (rc.isCoreReady()) {
-				for (Direction dir: DirectionHelper.directions) {
-					if(rc.senseOre(rc.getLocation().add(dir)) > rc.senseOre(rc.getLocation()) && rc.canMove(dir)) {
-						rc.move(dir);
-					}
-				}
-				rc.mine();
-			
 				int fate = rand.nextInt(1000);
 				if (rc.readBroadcast(200) == rc.getID()) {
 					rc.broadcast(200, 0);
@@ -37,24 +35,14 @@ public class Beaver extends Robot {
 					if (buildDirection != null && rc.canBuild(buildDirection, RobotType.BARRACKS)) {
 						rc.build(buildDirection, RobotType.BARRACKS);
 					}
-				} else if (fate < 600) {
-					rc.mine();
-				} else {
-					int intendedDirection;
-					if (fate < 900) {
-						intendedDirection = rand.nextInt(8);
-					} else {
-						intendedDirection = DirectionHelper.directionToInt(rc.getLocation().directionTo(rc.senseEnemyHQLocation()));
-					}
-					int offsetIndex = 0;
-					int[] offsets = {0,1,-1,2,-2};
-					while (offsetIndex < 5 && !rc.canMove(DirectionHelper.directions[(intendedDirection+offsets[offsetIndex]+8)%8])) {
-						offsetIndex++;
-					}
-					if (offsetIndex < 5) {
-						rc.move(DirectionHelper.directions[(intendedDirection+offsets[offsetIndex]+8)%8]);
+				} 
+				
+				for (Direction dir: DirectionHelper.directions) {
+					if(rc.senseOre(rc.getLocation().add(dir)) > rc.senseOre(rc.getLocation()) && rc.canMove(dir)) {
+						rc.move(dir);
 					}
 				}
+				rc.mine();
 			}
 		} catch (Exception e) {
 			System.out.println("Beaver Exception");
