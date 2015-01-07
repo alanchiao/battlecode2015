6f439f05486edeb2f10d9bfc8343151ca80068c5
Alan Chiao <alanchiao@mit.edu>
2015-01-06 22:52:01 -0500

    small prechanges for navigation

--- units/Unit.java
+++ units/Unit.java
@@ -1,9 +1,12 @@
 package battlecode2015.units;
+import battlecode.common.Direction;
 import battlecode.common.MapLocation;
 import battlecode2015.Robot;
 
 public abstract class Unit extends Robot{
 	// stored information about reaching a destination
 	// for navigation
-	protected MapLocation destinationPoint;
+	public MapLocation destinationPoint;
+	public Direction lastDirectionMoved;
+	boolean avoidingObstacle = false;
 }
--- units/com/Navigation.java
+++ units/com/Navigation.java
@@ -1,5 +1,15 @@
 package battlecode2015.units.com;
 
+import battlecode.common.Direction;
+import battlecode.common.RobotController;
+import battlecode2015.units.Unit;
+
 public class Navigation {
+	
+	// move unit steadliy towards destination
+	public static void moveToDestinationPoint(RobotController rc, Unit unit) {
+		Direction fastestDirection = rc.getLocation().directionTo(unit.destinationPoint);
+		
+	}
 
 }
