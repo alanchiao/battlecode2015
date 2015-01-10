50c97bdd72516abfb28747c43748b5f2d78afeeb
George Du <gdu@mit.edu>
2015-01-10 00:30:00 -0500

    replaced basher with drone shell

---
+++ units/Drone.java
@@ -0,0 +1,11 @@
+package team158.units;
+
+import battlecode.common.GameActionException;
+
+public class Drone extends Unit {
+
+	protected void actions() throws GameActionException {
+		
+		
+	}
+}
