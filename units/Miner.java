6a8dbf4b79137c07c14cb4b616730bf0a5357567
Alan Chiao <alanchiao@mit.edu>
2015-01-16 17:45:43 -0500

    mining optimal patch search: STOP

--- units/Miner.java
+++ units/Miner.java
@@ -37,7 +37,7 @@ public class Miner extends Unit {
 					possibleMovesAvoidingEnemies[i] = false;
 				}
 			}
-
+			
 			if (!possibleMovesAvoidingEnemies[8]) {
 				int dirint;
 				if (enemies.length == 0) { // need to avoid hq which is not in enemies
@@ -54,12 +54,14 @@ public class Miner extends Unit {
 					rc.move(DirectionHelper.directions[(dirint+offsets[offsetIndex]+8)%8]);
 				}
 			} else {
+				// TODO: account for tiles you cannot reach because attacked by enemyHQ/towers
+				// TODO: improve navigation & decrease cost
+				
 				// calculate average ore in 16 squared units
 				int numLocations = 0;
 				int sumOre = 0;
 				MapLocation nearbyLocations[] = myLocation.getAllMapLocationsWithinRadiusSq(myLocation, 16);
 				for (MapLocation location: nearbyLocations) {
-					
 					numLocations++;
 					sumOre += rc.senseOre(location);
 				}
@@ -71,13 +73,28 @@ public class Miner extends Unit {
 				rc.setIndicatorString(1, Integer.toString(currentIdealAverage));
 				rc.setIndicatorString(2, Boolean.toString(isLeaderMiner));
 				
-				/**
-				if (currentIdealAverage <= 2) {
-					ORE_THRESHOLD_TO_MOVE = 0.5;
-				} else if (currentIdealAverage >= 20) {
-					ORE_THRESHOLD_TO_MOVE = 10;
+				double maxNearbyOre = 0;
+				MapLocation bestLocation = null;
+				Direction bestDirection = null;
+				MapLocation nearbyOreLocations[] = myLocation.getAllMapLocationsWithinRadiusSq(myLocation, 2);
+				for (MapLocation location: nearbyOreLocations) {
+					if (location.equals(myLocation)) {
+						continue;
+					}
+					double oreThere = rc.senseOre(location);
+					if (oreThere > maxNearbyOre) {
+						maxNearbyOre = oreThere;
+						Direction direction = myLocation.directionTo(location);
+						if (possibleMovesAvoidingEnemies[DirectionHelper.directionToInt(direction)]) {
+							bestLocation = location;
+							bestDirection = direction;
+						}
+					}
 				}
-				**/
+				
+				if (currentIdealAverage <= 15) {
+					ORE_THRESHOLD_TO_MOVE = 0;
+				}
 				
 				if (isLeaderMiner) {
 					if (lastBroadcastedOreCount == currentIdealAverage) {
@@ -96,13 +113,19 @@ public class Miner extends Unit {
 					rc.broadcast(Broadcast.idealMiningOreAverage, averageOre);
 					lastBroadcastedOreCount = averageOre;
 					this.isLeaderMiner = true;
-					rc.mine();
+				}
 				// - decision to move to nice patch of ore. Once gets closer, its average will increase
 				// until it gets enough ore again.
 				// - only half a third of miners do the swarm
 				// - only do the swarm if the average is also higher than your ore on your patch
-				} else if (currentIdealAverage > averageOre * 2 && currentIdealAverage > myOre * 1.2 && rc.getID() % 3 == 0) {
+				if (currentIdealAverage > averageOre * 2.5 && 
+						   currentIdealAverage > myOre * 2    && 
+						   currentIdealAverage > maxNearbyOre * 2 &&
+						   rc.getID() % 3 == 0
+				) {
 					navigation.moveToDestination(betterLocation, true);
+				} else if (myOre <= ORE_THRESHOLD_TO_MOVE && bestLocation != null && maxNearbyOre > myOre) {
+					rc.move(bestDirection);
 				} else if (myOre <= ORE_THRESHOLD_TO_MOVE) {
 					// looks around for an ore concentration that is bigger than its current location by a certain fraction
 					// moves continuously in that direction until it reaches a non-zero ore area
