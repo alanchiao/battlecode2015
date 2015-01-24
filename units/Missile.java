df1bbf545c02b9ba13d08cc560aaee475aca593e
Alan Chiao <alanchiao@mit.edu>
2015-01-23 22:45:14 -0500

    account for missile less, fewer bytecodes

--- units/Missile.java
+++ units/Missile.java
@@ -1,13 +1,14 @@
 package team158.units;
 
+import team158.Robot;
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
 import battlecode.common.RobotInfo;
 import battlecode.common.RobotType;
-import team158.Robot;
 import team158.com.Broadcast;
-import team158.utils.DirectionHelper;
+import team158.utils.*;
+
 
 public class Missile extends Robot {
 		
@@ -23,54 +24,64 @@ public class Missile extends Robot {
 		try {
 			timeUntilDeath --;
 			if (rc.isCoreReady()) {
-				RobotInfo enemiesInAttack[] = rc.senseNearbyRobots(2, rc.getTeam().opponent());
-				boolean isNonMissileEnemyNear = false;
-				for (RobotInfo enemy: enemiesInAttack) {
-					if (enemy.type != RobotType.MISSILE) {
-						isNonMissileEnemyNear = true;
-						rc.explode();
-						return;
-					} 
+				RobotInfo unitsInAttack[] = rc.senseNearbyRobots(2);
+				int valuableAlliesInAttack = 0;
+				int effectiveEnemiesInAttack = 0;
+				for (RobotInfo unit: unitsInAttack) {
+					if (unit.type != RobotType.MISSILE) {
+						if (unit.team.equals(rc.getTeam())) {
+							valuableAlliesInAttack++;
+						} else {
+							effectiveEnemiesInAttack++;
+						}
+					}
 				}
 				
-				RobotInfo[] enemies = rc.senseNearbyRobots(24, rc.getTeam().opponent());
-				RobotInfo target = null;
-				for (RobotInfo enemy: enemiesInAttack) {
-					if (enemy.type != RobotType.MISSILE) {
-						target = enemy;
-						break;
-					} 
-				}
-				if (target != null) {
-					Direction moveDirection = rc.getLocation().directionTo(target.location);
-					Direction moveDirection2 = DirectionHelper.directions[(DirectionHelper.directionToInt(moveDirection) + 9) % 8];
-					Direction moveDirection3 = DirectionHelper.directions[(DirectionHelper.directionToInt(moveDirection) + 6) % 8];
-					if (rc.canMove(moveDirection)) {
-						rc.move(moveDirection);
-					} else if (rc.canMove(moveDirection2)) {
-						rc.move(moveDirection2);
-					} else  if (rc.canMove(moveDirection3)) {
-						rc.move(moveDirection3);
-					}
-					if (enemiesInAttack.length >= 3) {
-						rc.explode();
-					}
-				} else {
-					Direction moveDirection = rc.getLocation().directionTo(Broadcast.readLocation(rc, Broadcast.enemyHQLocation));
-					Direction moveDirection2 = DirectionHelper.directions[(DirectionHelper.directionToInt(moveDirection) + 9) % 8];
-					Direction moveDirection3 = DirectionHelper.directions[(DirectionHelper.directionToInt(moveDirection) + 6) % 8];
-					if (rc.canMove(moveDirection)) {
-						rc.move(moveDirection);
-					} else if (rc.canMove(moveDirection2)) {
-						rc.move(moveDirection2);
-					} else  if (rc.canMove(moveDirection3)) {
-						rc.move(moveDirection3);
-					}
-					if (enemiesInAttack.length >= 3) {
-						rc.explode();
-					}
-				}
-			   
+				if (effectiveEnemiesInAttack > 0 && valuableAlliesInAttack == 0) {
+					rc.explode();
+					return;
+				} 
+			   RobotInfo[] enemies = rc.senseNearbyRobots(24, rc.getTeam().opponent());
+			   if (enemies.length > 0) {
+				  Direction moveDirection = rc.getLocation().directionTo(enemies[0].location);
+				  if (rc.canMove(moveDirection)) {
+				          rc.move(moveDirection);
+				          return;
+				  }
+				  moveDirection = DirectionHelper.directions[(DirectionHelper.directionToInt(moveDirection) + 9) % 8];
+				  if (rc.canMove(moveDirection)) {
+				          rc.move(moveDirection);
+				          return;
+				  }
+				  moveDirection = DirectionHelper.directions[(DirectionHelper.directionToInt(moveDirection) + 6) % 8];
+				  if (rc.canMove(moveDirection)) {
+				          rc.move(moveDirection);
+				          return;
+				  }
+				  if (effectiveEnemiesInAttack - valuableAlliesInAttack >= 1) {
+					  rc.explode();
+				  }
+			   } else {
+				   Direction moveDirection = rc.getLocation().directionTo(Broadcast.readLocation(rc, Broadcast.enemyHQLocation));
+				  if (rc.canMove(moveDirection)) {
+				          rc.move(moveDirection);
+				          return;
+				  }
+				  moveDirection = DirectionHelper.directions[(DirectionHelper.directionToInt(moveDirection) + 9) % 8];
+				  if (rc.canMove(moveDirection)) {
+				          rc.move(moveDirection);
+				          return;
+				  }
+				  moveDirection = DirectionHelper.directions[(DirectionHelper.directionToInt(moveDirection) + 6) % 8];
+				  if (rc.canMove(moveDirection)) {
+				          rc.move(moveDirection);
+				          return;
+				  }
+				  if (effectiveEnemiesInAttack - valuableAlliesInAttack >= 1) {
+					  rc.explode();
+				  }	  
+			   }
+
 				if (timeUntilDeath == 1) {
 					rc.disintegrate();
 				}
